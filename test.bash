a6b6c534a8ebd7ca8eb6d2a95faa6ad52e9e1802
Jun <tokojun15@outlook.jp>
2023-11-02 12:11:37 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Jun Tokoeda
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -14,5 +14,20 @@ out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+exit $res
+### I/O ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
 exit $res
 
