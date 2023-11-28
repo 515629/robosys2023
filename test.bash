5fb42d9a385ecc67b7cbb712a3dbe0cba0f84336
Jun <tokojun15@outlook.jp>
2023-11-28 11:14:10 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -9,12 +9,6 @@ ng () {
 
 res=0
 
-### I/O TEST ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
-exit $res
 ### I/O ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
